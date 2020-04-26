3fb8b67318569e590e117c5db71f3c9579191639
shuangyangcoco <675312794@qq.com>
2020-04-27 07:39:48 +0800

    fix(core.py):add _make_apis function

--- swagger2case/core.py
+++ swagger2case/core.py
@@ -1,53 +1,198 @@
+from har2case import utils
+
 class SwaggerParser(object):
-    def gen_api(self, filt_type=json):
-        #create dictionary
-        _make_dirctionary_for_tags(tags_list)
-        apis_list = _make_apis(paths_dict)  #api have exists correct dir
-        for api in apis_list:
-            #api = {'tag_name', 'api_dict'}
-            if file_type = json:
-                utils.dump_json(api.api_dict, "api/pet/xxx.json")
-            else
-                utils.dump_yaml(api.api_dict, new_file_path)
 
-    def _make_dictionary_for_tags(self, tags_list):
-        for tag in tags_list:
-            #create folder for tag.name
+    def __init__(self, swagger_file_path, filter_str=None, exclude_str=None):
+        self.swagger_file_path = swagger_file_path
+        self.filter_str = filter_str
+        self.exclude_str = exclude_str or ""
 
 
-    def _make_apis(self, paths_dict):
-        apis_list = []
+    # def gen_api(self, filt_type=json):
+    #     #create
+    #     _make_dirctionar_for_tags(tags_list)
+    #     apis_list = _make_apis(paths_dict)  #api have exists correct dir
+    #     for api in apis_list:
+    #         #api = {'tag_name', 'api_dict'}
+    #         if file_type = json:
+    #             utils.dump_json(api.api_dict, "api/pet/xxx.json")
+    #         else
+    #             utils.dump_yaml(api.api_dict, new_file_path)
+
+
+
+
+
+
+    def _make_apis(self):
+        """ make api list.
+            api list are parsed from swagger paths dict.
+
+        """
+        def is_exclude(url, exclude_str):
+            exclude_str_list = exclude_str.split("|")
+            for exclude_str in exclude_str_list:
+                if exclude_str and exclude_str in url:
+                    return True
+
+            return False
+
+        api_list = []
+        swagger_all_info = load_swagger_api(self.swagger_file_path)
+        host = swagger_all_info['host']
+        basepath = swagger_all_info['basePath']
+        paths_dict = swagger_all_info['paths']
         for key, value in paths_dict:
-            # judge exclude/filter
-            (tag_name ,api_dict) = _make_api(key, value)
-            apis_list.append((tag_name ,api_dict))
-        return apis_list
 
-    def _make_api(self, key, value):
+            if self.filter_str and self.filter_str not in key:
+                continue
+
+            if is_exclude(key, self.exclude_str):
+                continue
+
+            else:
+                api_dict = _make_api(key, value, host, basepath)
+                api_list.append(api_dict)
+        return api_list
+
+    def _make_api(self, url_path, value, host, basepath):
+        """ extract info from path dict and make api
+
+                Args:
+                    "/pet": {
+                        "post": {
+                        "tags": [
+                            "pet"
+                        ],
+                        "summary": "Add a new pet to the store",
+                        "description": "",
+                        "operationId": "addPet",
+                        "consumes": [
+                            "application/json"],
+                        "produces": [
+                            "application/json" ],
+                        "parameters": [
+                        {
+                            "in": "body",
+                            "name": "body",
+                            "description": "Pet object that needs to be added to the store",
+                            "required": true,
+                            "schema": {
+                                "$ref": "#/definitions/Pet"
+                            }
+                        }],
+                       "responses": {
+                           "405": {
+                               "description": "Invalid input"
+                           }
+                       },
+                       "security": [
+                       {
+                           "petstore_auth": [
+                               "write:pets",
+                               "read:pets"
+                           ]
+                       }]
+                    },
+
+        """
+
+
         api_dict = {
-            "name":"",
+            "name": "",
             "variables": {},
-            "request":{},
-            "base_url":f"http://xxxxx/{key}",
-            "validate":[]
+            "request": {},
+            "base_url": "",
+            "validate": []
         }
-        _prepare_name(api_dict, value)
-        _prepare_variables(api_dict, value)
-        _prepare_request(api_dict, value)
-        _prepare_validate(api_dict, value)
+        self._prepare_name(api_dict, value)
+        self._prepare_variables(api_dict, value, basepath, host)
+        self._prepare_request(api_dict, url_path, value)
+        self._prepare_validate(api_dict, value)
         return api_dict
 
-    def _prepare_name(self,api_dict ,value):
+    def _prepare_name(self, api_dict, value):
+        '''
+        parser operationId info from path dict and make name of api
+        '''
+        api_dict['name'] = value['operationId']
+
+    def _prepare_variables(self, api_dict, value):
         pass
 
-    def _prepare_request(self,api_dict, value):
-        _make_request_url(api_dict, value)
+    def _prepare_validate(self, api_dict, value):
+        pass
+
+    def _prepare_request(self,api_dict, url_path, value):
+        """ extract info from path dict and make request.
+
+            Args:
+                "/pet": {
+                    "post": {
+                    "summary": "Add a new pet to the store",
+                    "consumes": [
+                        "application/json"],
+                    "produces": [
+                        "application/json" ],
+                    "parameters": [
+                    {
+                        "in": "body",
+                        "name": "body",
+                        "description": "Pet object that needs to be added to the store",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/Pet"
+                        }
+                    }],
+                },
+        """
+
+        _make_request_url(api_dict, url_path, value)
         _make_request_method(api_dict, value)
         _make_request_headers(api_dict, value)
         _make_request_data(api_dict, value)
 
-    def _prepare_validate(self,api_dict, value):
-        pass
+    def __make_request_url(self, api_dict, url_path, value):
+        """ parse url and parameters, and make url and params of api request
+
+                Args:
+                    "/pet/findByStatus": {
+                        "get": {
+                          "parameters": [
+                            {
+                              "name": "status",
+                              "in": "query",
+                              "required": true,
+                              "type": "array",
+                              "items": {
+                                "type": "string",
+                                "enum": [
+                                  "available",
+                                  "pending",
+                                  "sold"
+                                ],
+                                "default": "available"
+                              },
+                              "collectionFormat": "multi"
+                            }
+                          ],
+                Returns:
+                    {
+                        "request": {
+                            url: "/pet/findByStatus",
+                            params: {"name": "sold"}
+                        }
+                    }
+
+        """
+
+
+
+
+
+
+
+
 
 
 
