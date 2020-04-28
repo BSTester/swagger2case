289248769a7a98c1c1ed45525b9aa2bc188a0ee6
shuangyangcoco <675312794@qq.com>
2020-04-29 07:58:45 +0800

    fix(test_swagger.py):fix test make api

--- swagger2case/test_swagger.py
+++ swagger2case/test_swagger.py
@@ -1,16 +1,17 @@
+import pytest
 from swagger2case.core import SwaggerParser
 
 class TestSwagger(object):
 
     def setup_class(self):
-        self.swagger_parser = SwaggerParser()
+        self.swagger_parser = SwaggerParser("tests/data/demo.json")
+        self.method_value = self.swagger_parser.swagger_all_info["paths"]["/pet"]["post"]
 
     def test_make_api(self):
-        swagger_paths_dict = load_swagger_api_paths(self.swagger_api_path)
-        api_dict = self.swagger_parser._make_api("/pet", swagger_paths_dict["/pet"])
-        assert "name" in api_dict
-        assert "request" in api_dict
-        assert "validate" in api_dict
+        api_dict = self.swagger_parser._make_api("/pet", "post", self.method_value)
+        assert "name" in api_dict[1]
+        assert "request" in api_dict[1]
+        assert "validate" in api_dict[1]
 
         validators_mapping = {
             validator["eq"][0]: validator["eq"][1]
