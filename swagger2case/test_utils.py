f6b559893288f19c4cd5652a34a23acd6f59877f
shuangyangcoco <675312794@qq.com>
2020-05-15 07:50:51 +0800

    test(test_utils): add test code for generate_validate_content_with_definitions

--- swagger2case/test_utils.py
+++ swagger2case/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from loguru import logger
-from swagger2case.utils import get_related_tag_definitions_content, load_swagger_api
+from swagger2case.utils import get_related_tag_definitions_content, load_swagger_api, generate_validate_content
 
 class TestUtils(object):
 
@@ -21,3 +21,28 @@ class TestUtils(object):
             'photoUrls': [],
             'tags': [],
             'status': ''}
+
+    def test_generate_validate_content_with_definitions(self):
+        definitions_dict = {
+            "id": 0,
+            "category": {
+                "id": 0,
+                "name": ""
+            },
+            "name": "",
+            "photoUrls": [],
+            "tags": [],
+            "status": ""}
+
+        validate_content = generate_validate_content(definitions_dict)
+        logger.exception(validate_content)
+        assert validate_content == [
+                {"eq": ["content.id", 0]},
+                {"eq": ["content.category.id", 0]},
+                {"eq": ["content.category.name", '']},
+                {"eq": ["content.name", '']},
+                {"eq": ["content.photoUrls", []]},
+                {"eq": ["content.tags", []]},
+                {"eq": ["content.status", '']},
+
+        ]
